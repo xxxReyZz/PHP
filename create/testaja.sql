2e4eae529556e366354addcb8a1e5530727f6e04
xxxReyZz <97242958+xxxReyZz@users.noreply.github.com>
2023-10-10 13:44:30 +0700

    Add files via upload

---
+++ create/testaja.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 10, 2023 at 08:43 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `testaja`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `id` int(11) NOT NULL,
+  `nik` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `jabatan` varchar(50) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `gambar` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `karyawan`
+--
+
+INSERT INTO `karyawan` (`id`, `nik`, `nama`, `jabatan`, `email`, `gambar`) VALUES
+(1, 'Excalibur Sword', 'King Reynaldi', 'Baginda Raja', 'Warmblood Horse', 'reynaldi.jpeg'),
+(2, 'Bastard Sword', 'Azhar', 'Panglima Perang', 'Arabian Horse', 'azhar.jpeg'),
+(3, 'Zweihander Sword', 'Dede', 'Penasihat Raja', 'Andalusian Horse', 'dede.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `email` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
+(1, 'reynaldi', '$2y$10$tglYF9XCX4acUTTQxiXlBOIAx', 'reynaldisetiaparhan@gmail.com'),
+(6, 'rey', '$2y$10$Fi.Nvic7.QyFnxbqvVjtG.h3U', 'reynaldisetiafarhan73@gmail.com'),
+(7, 'admin', '$2y$10$B3eYq4YlQwNnItwlKuapTOwQD', 'admin@gmail.com'),
+(8, 'ada', '$2y$10$NIrGowdnZU/4tB7oHPQOsOahR', 'ada@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
